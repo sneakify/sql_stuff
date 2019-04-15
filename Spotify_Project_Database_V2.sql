21d16d7da3cbf9582f110f719ddb0ca2831fd55c
mathewglee <49292983+mathewglee@users.noreply.github.com>
2019-04-15 13:53:18 -0400

    Add files via upload

--- Spotify_Project_Database_V2.sql
+++ Spotify_Project_Database_V2.sql
@@ -1,43 +1,64 @@
 drop database if exists spootify;
 create database spootify;
 use spootify;
-drop table if exists user;
-drop table if exists buy;
-drop table if exists sell;
-drop table if exists top_50;
-    
+
+create table genre (
+	genre_id int primary key,
+    genre_name varchar(30)
+    );
+ 
+
+insert into genre values
+	(1, 'country'),
+    (2, 'alternative'),
+    (3, 'hip-hop'),
+    (4, 'pop');
     
  create table artist (
 	artist_id int primary key,
     artist_name varchar(75) not null,
-    genre varchar(30) not null
+    genre_id int not null,
+    constraint fk_genre_id_artist foreign key(genre_id) references genre(genre_id)
     );
 
 insert into artist values
-	(1, 'Bob', 'trance');
+	(1, 'Lil Nas X', 1),
+    (2, 'Billie Eilish', 2),
+    (3, 'J. Cole', 3),
+    (4, 'Post Malone', 3),
+    (5, 'Ariana Grande', 4);
+    
     
 create table album (
 	album_id int primary key,
     album_name varchar(75)
     );
+    
 
 insert into album values
-	(1, 'album1');
+	(1, 'Old Town Road'),
+    (2, 'When We All Fall'),
+    (3, 'Middle Child - Single'),
+    (4, 'Sunflower - Spider-Man: Into the Spider-Verse'),
+    (5, 'thank u, next');
+    ;
     
 create table song(
 	spotify_id int primary key,
     title varchar(75) not null,
     artist_id int not null,
     album_id int not null,
-    song_rank int not null,
+    song_value int not null,
     constraint fk_artist_id_song foreign key(artist_id) references artist(artist_id),
     constraint fk_album_id_song foreign key(album_id) references album(album_id)
     );
     
 insert into song values
-	(1, 'song1', 1, 1, 100),
-    (2, 'song2', 1, 1, 20),
-    (3, 'song3', 1, 1, 0);
+	(1, 'Old Town Road - Remix', 1, 1, 293),
+    (2, 'bad guy', 2, 2, 193),
+    (3, 'wish you were gay', 2, 2, 112),
+    (4, 'ilomilo', 2, 2, 40),
+    (5, 'Middle Child', 3, 3, 112);
     
     
     
@@ -47,6 +68,26 @@ create table song_history (
     day_value int not null,
     constraint fk_spotify_id_song_history foreign key(spotify_id) references song(spotify_id)
     );
+    
+insert into song_history values 
+    ('2019-04-08', 1, 311),
+    ('2019-04-08', 2, 202),
+    ('2019-04-08', 3, 118),
+    ('2019-04-08', 4, 53),
+    ('2019-04-08', 5, 108),
+    ('2019-04-09', 1, 310),
+    ('2019-04-09', 2, 197),
+    ('2019-04-09', 3, 115),
+    ('2019-04-09', 4, 51),
+    ('2019-04-09', 5, 111),
+    ('2019-04-10', 1, 293),
+    ('2019-04-10', 2, 187),
+    ('2019-04-10', 3, 116),
+    ('2019-04-10', 4, 49),
+    ('2019-04-10', 5, 121);
+    
+    
+    
 
 create table user (
 	user_id int primary key auto_increment,
@@ -60,7 +101,7 @@ create table user (
 
 insert into user values
 	(1, 'm', 'm', 'm@m.com', 'password', '2019-04-08 03:02:54', 5000),
-    (2, 'n', 'n', 'n@n.com', 'password', '2019-04-08 03:02:54', 10);
+    (2, 'n', 'n', 'n@n.com', 'password', '2019-04-08 03:02:54', 5000);
 
 create table user_history (
 	date date not null,
@@ -70,7 +111,7 @@ create table user_history (
     );
     
 create table buy (
-	buy_id int primary key,
+	buy_id int primary key auto_increment,
 	user_id int not null,
     spotify_id int not null,
     price int not null,
@@ -80,15 +121,10 @@ create table buy (
     constraint fk_spotify_id_buy foreign key(spotify_id) references song(spotify_id)
     );
     
-insert into buy values
-	(1, 1, 1, 100, 5, '2019-04-08 12:02:54'),
-    (2, 1, 2, 50, 20, '2019-04-08 12:02:54'),
-    (3, 1, 1, 100, 5, '2019-04-08 12:02:54'),
-    (4, 2, 2, 50, 1, '2019-04-08 12:02:54');
     
     
 create table sell (
-	sell_id int primary key,
+	sell_id int primary key auto_increment,
 	user_id int not null,
     spotify_id int not null,
     price int not null,
@@ -98,32 +134,6 @@ create table sell (
     constraint fk_spotfiy_id_sell foreign key(spotify_id) references song(spotify_id)
     );
     
-insert into sell values
-	(1, 1, 1, 95, 5, '2019-04-08 14:02:54'),
-    (2, 1, 2, 50, 10, '2019-04-08 14:02:54');
-
-Select user_id, spotify_id, sum(n_shares) as user_bought
-From buy
-Group by user_id, spotify_id;
-
-Select user_id, spotify_id, sum(n_shares) as user_sold
-From sell
-Group by user_id, spotify_id;  
-
-
-
-
-
-Select user_id, spotify_id, user_bought - coalesce(user_sold,0) as shares_owned
-		From 
-			(Select user_id, spotify_id, sum(n_shares) as user_bought
-			From buy
-			Group by user_id, spotify_id) as total_bought
-		Left Join 
-			(Select user_id, spotify_id, sum(n_shares) as user_sold
-			From sell
-			Group by user_id, spotify_id) as total_sold
-			Using (user_id, spotify_id);
 
 
             
@@ -131,16 +141,18 @@ Select user_id, spotify_id, user_bought - coalesce(user_sold,0) as shares_owned
 Drop Trigger If Exists buy_triggers;
 
 Delimiter //
-
 Create Trigger buy_triggers
 	Before Insert On buy
     For Each Row
 Begin
-	If ((New.price * New.n_shares) > user.purchasing_power) Then
+	If ((New.price * New.n_shares) > (Select purchasing_power as u_power
+										From user
+										Where New.user_id = user.user_id))
+		Then
 		Signal Sqlstate 'HYOOO'
 			Set Message_Text = 'Insufficient Funds';
 	End If;
-    If (time(New.purchase_time) < '9:30' Or time(New.purchase_time) > '16:30') Then
+    If (time(New.purchase_time) < '09:30' Or time(New.purchase_time) > '16:30') Then
 		Signal Sqlstate 'HYOOO'
 			Set Message_Text = 'Market Closed';
 	Else
@@ -150,9 +162,6 @@ Begin
 	End If;
 End //
 
--- buy trigger testing
-
--- selling trigger
 
 Delimiter //
 
@@ -163,7 +172,7 @@ RETURNS INT
 BEGIN
 DECLARE bought INT;
 
-Select user_id, sum(n_shares) into bought
+Select sum(n_shares) into bought
 From buy
 Where u_id = user_id And s_id = spotify_id
 Group by user_id, spotify_id;
@@ -180,7 +189,7 @@ RETURNS INT
 BEGIN
 DECLARE sold INT;
 
-Select user_id, sum(n_shares) into sold
+Select sum(n_shares) into sold
 From sell
 Where u_id = user_id And s_id = spotify_id
 Group by user_id, spotify_id;
@@ -203,7 +212,7 @@ Begin
 		Signal Sqlstate 'HYOOO'
 			Set Message_Text = 'Not Enough Shares';
 	End If;
-    If (time(New.sale_time) < '9:30' Or time(New.sale_time) > '16:30') Then
+    If (time(New.sale_time) < '09:30' Or time(New.sale_time) > '16:30') Then
 		Signal Sqlstate 'HYOOO'
 			Set Message_Text = 'Market Closed';
 	Else
@@ -212,17 +221,7 @@ Begin
         Where user.user_id = New.user_id;
 	End If;
 End //
-    
-
--- resets song value every day
-create event reset_song_value
-	on schedule every 1 day
-	starts '2019-01-01 00:03:00' 
-do
-	update song
-    set date = curdate(),
-		day_value = 0;
-
+ 
 
 -- updates song history every day
 create event update_song_history
@@ -230,20 +229,20 @@ create event update_song_history
 	starts '2019-01-01 00:05:00' 
 do
 	insert into song_history
-    select curdate(), spotify_id, song_rank
+    select curdate(), spotify_id, song_value
     from song;
 
 -- updates user history every day
 create event update_user_history
 	on schedule every 1 day
-	starts '2019-01-01 00:09:30'
+	starts '2019-01-01 00:09:00'
 do
 	insert into user_history
     select curdate(), user_id, portfolio_value
     from 
     (Select user_id, (sum(total_value) + purchasing_power) as portfolio_value
 		From
-		(Select user_id, spotify_id, shares_owned * song_rank as total_value
+		(Select user_id, spotify_id, shares_owned * song_value as total_value
 		From
 			(Select user_id, spotify_id, user_bought - coalesce(user_sold,0) as shares_owned
 			From 
@@ -260,6 +259,102 @@ do
 	Group by user_id) as daily_portfolio
 Where user.user_id = daily_portfolio.user_id;
 
+ 
+Delimiter //
+insert into buy values
+	(1, 1, 1, 311, 5, '2019-04-08 12:02:54'),
+    (2, 1, 5, 108, 6, '2019-04-08 12:02:54'),
+    (3, 1, 5, 111, 5, '2019-04-09 12:02:54'),
+    (4, 2, 2, 116, 3, '2019-04-10 12:02:54');
+
+insert into sell values
+	(1, 1, 1, 293, 2, '2019-04-10 14:02:54'),
+    (2, 1, 5, 121, 10, '2019-04-10 14:02:54');
+    
+Delimiter //
+    
+Select user_id, spotify_id, sum(n_shares) as user_bought
+From buy
+Group by user_id, spotify_id;
+
+Select user_id, spotify_id, sum(n_shares) as user_sold
+From sell
+Group by user_id, spotify_id;  
 
 
+Select user_id, spotify_id, user_bought - coalesce(user_sold,0) as shares_owned
+		From 
+			(Select user_id, spotify_id, sum(n_shares) as user_bought
+			From buy
+			Group by user_id, spotify_id) as total_bought
+		Left Join 
+			(Select user_id, spotify_id, sum(n_shares) as user_sold
+			From sell
+			Group by user_id, spotify_id) as total_sold
+			Using (user_id, spotify_id);
+            
 
+
+-- last 7 days of history of given user
+Select date, portfolio_value
+From user_history
+Where user_id = @
+Order by date
+Limit 7;
+
+-- returns the value of the given song
+Select song_value
+From song
+Where song_id = @;
+
+-- all # of shares given user owns
+Select title, user_bought - coalesce(user_sold,0)
+			From 
+				(Select user_id, spotify_id, sum(n_shares) as user_bought
+				From buy
+				Group by user_id, spotify_id) as total_bought
+			Left Join 
+				(Select user_id, spotify_id, sum(n_shares) as user_sold
+				From sell
+				Group by user_id, spotify_id) as total_sold
+				Using (user_id, spotify_id)) as user_total
+		Join song Using (spotify_id)) as user_share_value;
+
+-- value and number of shares given user owns
+Select title, user_bought - coalesce(user_sold,0), shares_owned * song_value 
+		From
+			(Select user_id, spotify_id, user_bought - coalesce(user_sold,0) as shares_owned
+			From 
+				(Select user_id, spotify_id, sum(n_shares) as user_bought
+				From buy
+				Group by user_id, spotify_id) as total_bought
+			Left Join 
+				(Select user_id, spotify_id, sum(n_shares) as user_sold
+				From sell
+				Group by user_id, spotify_id) as total_sold
+				Using (user_id, spotify_id)) as user_total
+		Join song Using (spotify_id)) as user_share_value;
+        
+-- history of given song value past 7 days
+Select date, day_value
+From song_history
+Where song_id = @;
+
+-- list of songs from given artist
+Select *
+From song
+Join artist Using (artist_id)
+Where artist_name Like @;
+
+-- list of songs from given genre
+Select *
+From song
+Join artist Using (artist_id)
+Join genre Using (genre_id)
+Where genre_name Like @;
+
+-- list of songs from given album
+Select *
+From song
+Join album Using (artist_id)
+Where album_name Like @;
